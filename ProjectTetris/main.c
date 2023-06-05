52318f3fc24bc525370ae3f48f53ea12b7b66cb6
yang920916 <yang920916@gmail.com>
2023-06-06 00:50:03 +0800

    6/6final

--- ProjectTetris/main.c
+++ ProjectTetris/main.c
@@ -4,6 +4,21 @@
 #include <time.h>
 #include <windows.h>
 
+#define LEFT_KEY 0x25     // The key to move left, default = 0x25 (left arrow)
+#define RIGHT_KEY 0x27    // The key to move right, default = 0x27 (right arrow)
+#define ROTATE_KEY 0x26   // The key to rotate, default = 0x26 (up arrow)
+#define DOWN_KEY 0x28     // The key to move down, default = 0x28 (down arrow)
+#define FALL_KEY 0x20     // The key to fall, default = 0x20 (spacebar)
+
+#define FALL_DELAY 500    // The delay between each fall, default = 500
+#define RENDER_DELAY 100  // The delay between each frame, default = 100
+
+#define LEFT_FUNC() GetAsyncKeyState(LEFT_KEY) & 0x8000
+#define RIGHT_FUNC() GetAsyncKeyState(RIGHT_KEY) & 0x8000
+#define ROTATE_FUNC() GetAsyncKeyState(ROTATE_KEY) & 0x8000
+#define DOWN_FUNC() GetAsyncKeyState(DOWN_KEY) & 0x8000
+#define FALL_FUNC() GetAsyncKeyState(FALL_KEY) & 0x8000
+
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
 
@@ -308,24 +323,229 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
 {
     printf("\033[0;0H\n");
     for (int i = 0; i < CANVAS_HEIGHT; i++) {
-        printf("|");
+        printf("\x1b[44m|");
         for (int j = 0; j < CANVAS_WIDTH; j++) {
             printf("\033[%dm\u3000", canvas[i][j].color);
         }
-        printf("\033[0m|\n");
+        printf("\x1b[44m|\n");
+    }
+
+    Shape shapeData = shapes[state->queue[1]];
+    printf("\033[%d;%dHNext:", 3, CANVAS_WIDTH * 2 + 5);
+    for (int i = 1; i <= 3; i++)
+    {
+        shapeData = shapes[state->queue[i]];
+        for (int j = 0; j < 4; j++) {
+            printf("\033[%d;%dH", i * 4 + j, CANVAS_WIDTH * 2 + 15);
+            for (int k = 0; k < 4; k++) {
+                if (j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k]) {
+                    printf("\x1b[%dm  ", shapeData.color);
+                }
+                else {
+                    printf("\x1b[0m  ");
+                }
+            }
+        }
     }
     return;
 }
 
+int clearLine(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH]) {
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            if (canvas[i][j].current) {
+                canvas[i][j].current = false;
+            }
+        }
+    }
+
+    int linesCleared = 0;
+
+    for (int i = CANVAS_HEIGHT - 1; i >= 0; i--)
+    {
+        bool isFull = true;
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
+            if (canvas[i][j].shape == EMPTY) {
+                isFull = false;
+                break;
+            }
+        }
+
+        if (isFull) {
+            linesCleared += 1;
+
+            for (int j = i; j > 0; j--)
+            {
+                for (int k = 0; k < CANVAS_WIDTH; k++)
+                {
+                    setBlock(&canvas[j][k], canvas[j - 1][k].color, canvas[j - 1][k].shape, false);
+                    resetBlock(&canvas[j - 1][k]);
+                }
+            }
+            i++;
+        }
+    }
+
+
+    return linesCleared;
+}
+
 void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
 {
-    if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
-        state->y++;
+    if (ROTATE_FUNC()) {
+        int newRotate = (state->rotate + 1) % 4;
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, newRotate, state->queue[0]))
+        {
+            state->rotate = newRotate;
+        }
+    }
+    else if (LEFT_FUNC()) {
+        if (move(canvas, state->x, state->y, state->rotate, state->x - 1, state->y, state->rotate, state->queue[0]))
+        {
+            state->x -= 1;
+        }
+    }
+    else if (RIGHT_FUNC()) {
+        if (move(canvas, state->x, state->y, state->rotate, state->x + 1, state->y, state->rotate, state->queue[0]))
+        {
+            state->x += 1;
+        }
+    }
+    else if (DOWN_FUNC()) {
+        state->fallTime = FALL_DELAY;
+    }
+    else if (FALL_FUNC()) {
+        state->fallTime += FALL_DELAY * CANVAS_HEIGHT;
+    }
+
+    state->fallTime += RENDER_DELAY + state->score * 200;
+
+    while (state->fallTime >= FALL_DELAY) {
+        state->fallTime -= FALL_DELAY;
+
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0])) {
+            state->y++;
+        }
+        else {
+            state->score += clearLine(canvas);
+
+            state->x = CANVAS_WIDTH / 2;
+            state->y = 0;
+            state->rotate = 0;
+            state->fallTime = 0;
+            state->queue[0] = state->queue[1];
+            state->queue[1] = state->queue[2];
+            state->queue[2] = state->queue[3];
+            state->queue[3] = rand() % 7;
+        }
+    }
     return;
+}
+
+void display(void)
+{
+    system("cls");
+    printf("\033[%d;%dH H", 8, 11);
+    Sleep(500);
+    printf("\033[%d;%dH E", 8, 13);
+    Sleep(500);
+    printf("\033[%d;%dH L", 8, 15);
+    Sleep(500);
+    printf("\033[%d;%dH L", 8, 17);
+    Sleep(500);
+    printf("\033[%d;%dH O", 8, 19);
+    Sleep(500);
+    printf("\033[%d;%dHWelcome to the Tetris game!\n", 9, 5);
+    printf("\n\n\t");
+    Sleep(1000);
+    system("pause", 10, 15);
+
+}
+
+void run()
+{
+    system("cls");
+
+    printf("\033[%d;%dH 3", 8, 15);
+    Sleep(1000);
+    system("cls");
+    printf("\033[%d;%dH 2", 8, 15);
+    Sleep(1000);
+    system("cls");
+    printf("\033[%d;%dH 1", 8, 15);
+    Sleep(1000);
+    system("cls");
+    printf("\033[%d;%dH \x1b[32m START!", 8, 11);
+    Sleep(1000);
+}
+
+void advise(State* state)
+{
+    if (state->score == 0)
+    {
+        printf("\033[%d;%dH\x1b[33m You are a Loser!", 2, CANVAS_WIDTH * 2 + 4);
+    }
+    if (state->score == 1)
+    {
+        printf("\033[%d;%dH\x1b[33m LOL please harder", 2, CANVAS_WIDTH * 2 + 4);
+    }
+    if (state->score == 2)
+    {
+        printf("\033[%d;%dH\x1b[33m QQ so sad.      ", 2, CANVAS_WIDTH * 2 + 4);
+    }
+    if (state->score == 3)
+    {
+        printf("\033[%d;%dH\x1b[33m Wow nice.    ", 2, CANVAS_WIDTH * 2 + 4);
+    }
+    if (state->score > 4)
+    {
+        printf("\033[%d;%dH Orz...orz..orz", 2, CANVAS_WIDTH * 2 + 4);
+    }
+}
+
+void ending(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
+{
+    if (!move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0]))
+    {
+        system("cls");
+
+        printf("\n\n");
+        printf("\t");
+        printf("\033[%d;%dH\x1b[41m GAME OVER \x1b[0m\033[%d;%dH", CANVAS_HEIGHT - 10, 17, CANVAS_HEIGHT, 0);
+        printf("\033[%d;%dHyour score: %d", 11, 17, state->score);
+        Sleep(3000);
+        printf("\n\t\t");
+        system("cls");
+        printf("\033[%d;%dH\x1b[31m F", 8, 11);
+        Sleep(500);
+        printf("\033[%d;%dH\x1b[31m A", 8, 13);
+        Sleep(500);
+        printf("\033[%d;%dH\x1b[31m I", 8, 15);
+        Sleep(500);
+        printf("\033[%d;%dH\x1b[31m L", 8, 17);
+        Sleep(500);
+        printf("\n\n\n\t");
+        system("pause");
+        exit(0);
+    }
+}
+
+void hideCursor()
+{
+    // ÁôÂÃ¹«¼Ð
+    CONSOLE_CURSOR_INFO cursor;
+    cursor.bVisible = false;
+    cursor.dwSize = sizeof(cursor);
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorInfo(handle, &cursor);
 }
 
 int main()
 {
+    hideCursor();
+    display();
+    run();
     srand(time(NULL));
     State state = {
         .x = CANVAS_WIDTH / 2,
@@ -335,12 +555,14 @@ int main()
         .fallTime = 0
     };
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 4; i++)
     {
         state.queue[i] = rand() % 7;
     }
 
     Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+    int score = clearLine(canvas);
+
     for (int i = 0; i < CANVAS_HEIGHT; i++)
     {
         for (int j = 0; j < CANVAS_WIDTH; j++)
@@ -350,15 +572,20 @@ int main()
     }
 
     system("cls");
-    printf("\e[?25l"); // hide cursor
+    // printf("\e[?25l"); // hide cursor
 
     move(canvas, state.x, state.y, state.rotate, state.x, state.y, state.rotate, state.queue[0]);
 
     while (1)
     {
+        printf("\033[%d;%dH \x1b[;33;1mScore: %d", 0, CANVAS_WIDTH * 2 + 4, state.score);
+        advise(&state);
         logic(canvas, &state);
         printCanvas(canvas, &state);
         Sleep(100);
+        ending(canvas, &state);
     }
 
 }
+
+
